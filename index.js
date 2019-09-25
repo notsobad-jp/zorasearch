c072b84a66eab856e097cb3ffd27a660ea7dbd67
tomomichi <tomomichi.onishi@gmail.com>
2019-09-25 17:17:12 +0900

    Fix paging without AMP List

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const pug = require('pug');
 const fs = require("fs");
 
 let json = fs.readFileSync('books.json');
-let books = JSON.parse(json);
+let results = JSON.parse(json);
 
 let params = {
   meta: {
     title: "hogehoge"
   },
-  results: books
+  links: {},
+  results: results
+}
+
+const currentUrl = "http://127.0.0.1:8887/output.html";
+if(results.links) {
+  const afterMatch = (results.links.next || "").match(/[?&]after=([^&]+)/);
+  if(afterMatch) { params.links.next = `${currentUrl}?after=${afterMatch[1]}`; }
+
+  const beforeMatch = (results.links.prev || "").match(/[?&]before=([^&]+)/);
+  if(beforeMatch) { params.links.prev = `${currentUrl}?before=${beforeMatch[1]}`; }
 }
 
 const html = pug.renderFile('index.pug', params);
