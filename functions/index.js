d04a52de236a3a46c1a1e31a72c3b1b11e2266d3
tomomichi <tomomichi.onishi@gmail.com>
2019-10-05 18:58:30 +0900

    WIP: set meta tag

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const pug = require('pug');
 const express = require('express');
 
 const app = express();
-app.get('/timestamp', (request, response) => {
+app.get('/', (request, response) => {
   // response.send(`${Date.now()}`);
   fetch('https://api.bungomail.com/v0/books?limit=10')
     .then((res) => {
@@ -33,5 +33,92 @@ app.get('/timestamp', (request, response) => {
       response.status(200).send(html);
     });
 })
+
+app.get('/authors/:author_id/categories/:category/books', (req, res) => {
+  const baseUrl = "https://api.bungomail.com/v0";
+  let url = (req.params.author_id == 'all') ? `${baseUrl}/books` : `${baseUrl}/persons/${req.params.author_id}/books`;
+  if(req.params.category != 'all') { req.query[encodeURIComponent('カテゴリ')] = req.params.category; }
+  const queries = Object.keys(req.query).map((m) => `${m}=${req.query[m]}`).join("&");
+  if(queries) { url = `${url}?${queries}`; }
+  fetch(url)
+    .then((response) => {
+      if(!response.ok) { return res.status(500).send(response); }
+      return response.json();
+    })
+    .then((json) => {
+      let params = {
+        meta: metaData(json.person, req.params.category),
+        category: req.params.category,
+        author_id: req.params.author_id,
+        links: {},
+        results: json
+      }
+
+      if(json.links) {
+        const afterMatch = (json.links.next || "").match(/[?&]after=([^&]+)/);
+        if(afterMatch) { params.links.next = `${req.path}?after=${afterMatch[1]}`; }
+
+        const beforeMatch = (json.links.prev || "").match(/[?&]before=([^&]+)/);
+        if(beforeMatch) { params.links.prev = `${req.path}?before=${beforeMatch[1]}`; }
+      }
+
+      const html = pug.renderFile('views/index.pug', params);
+      res.status(200).send(html);
+    });
+})
+
+const metaData = (person, category) => {
+  const meta = {
+    title: title(person, category),
+    description: description(person, category)
+  };
+  return meta;
+}
+
+const title = (person, category) => {
+  const authorName = (person) ? person["姓名"] : '青空文庫';
+  let title = "";
+  if(category != 'all') {
+    title += `${categoryReadTime(category)}で読める`;
+  }
+  title += `${authorName}の${categoryName(category)}作品`;
+  return title;
+}
+
+const description = (person, category) => {
+  const authorName = (person) ? person["姓名"] : 'すべて';
+  let description = "";
+  description += `青空文庫で公開されている${authorName}の作品`;
+  if(category != 'all') {
+    description += `の中で、おおよその読了目安時間が「${categoryReadTime(category)}」の${categoryName(category)}132作品`;
+  }
+  description += `を、おすすめ人気順に表示しています。`;
+  return description;
+}
+
+const categoryName = (id) => {
+  switch (id) {
+    case 'all': return '全';
+    case 'flash': return '短編';
+    case 'shortshort': return '短編';
+    case 'short': return '短編';
+    case 'novelette': return '中編';
+    case 'novel': return '長編';
+    default: return '';
+  }
+}
+
+const categoryReadTime = (id) => {
+  switch (id) {
+    case 'flash': return '5分以内';
+    case 'shortshort': return '10分以内';
+    case 'short': return '30分以内';
+    case 'novelette': return '60分';
+    case 'novel': return '1時間〜';
+    default: return '';
+  }
+}
+
+
 
 exports.app = functions.https.onRequest(app);
