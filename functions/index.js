33af3bf78d0e8dd600234f137b7510b93eba2e06
tomomichi <tomomichi.onishi@gmail.com>
2019-10-05 14:31:03 +0900

    Refactor with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const fetch = require("node-fetch");
+const pug = require('pug');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+app.get('/timestamp', (request, response) => {
+  // response.send(`${Date.now()}`);
+  fetch('https://api.bungomail.com/v0/books?limit=10')
+    .then((res) => {
+      if(!res.ok) { throw new Error(); }
+      return res.json();
+    })
+    .then((json) => {
+      let params = {
+        meta: {
+          title: "hogehoge"
+        },
+        links: {},
+        results: json
+      }
+
+      const currentUrl = "http://127.0.0.1:8887/output.html";
+      if(json.links) {
+        const afterMatch = (json.links.next || "").match(/[?&]after=([^&]+)/);
+        if(afterMatch) { params.links.next = `${currentUrl}?after=${afterMatch[1]}`; }
+
+        const beforeMatch = (json.links.prev || "").match(/[?&]before=([^&]+)/);
+        if(beforeMatch) { params.links.prev = `${currentUrl}?before=${beforeMatch[1]}`; }
+      }
+
+      const html = pug.renderFile('views/index.pug', params);
+      response.status(200).send(html);
+    });
+})
+
+exports.app = functions.https.onRequest(app);
