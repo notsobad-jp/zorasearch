423af683b0575a1f282ffd5fc588555f0b9620d0
tomomichi <tomomichi.onishi@gmail.com>
2019-10-07 16:51:00 +0900

    Add show page

--- functions/index.js
+++ functions/index.js
@@ -4,36 +4,8 @@ const pug = require('pug');
 const express = require('express');
 
 const app = express();
-app.get('/', (request, response) => {
-  // response.send(`${Date.now()}`);
-  fetch('https://api.bungomail.com/v0/books?limit=10')
-    .then((res) => {
-      if(!res.ok) { throw new Error(); }
-      return res.json();
-    })
-    .then((json) => {
-      let params = {
-        meta: {
-          title: "hogehoge"
-        },
-        links: {},
-        results: json
-      }
 
-      const currentUrl = "http://127.0.0.1:8887/output.html";
-      if(json.links) {
-        const afterMatch = (json.links.next || "").match(/[?&]after=([^&]+)/);
-        if(afterMatch) { params.links.next = `${currentUrl}?after=${afterMatch[1]}`; }
-
-        const beforeMatch = (json.links.prev || "").match(/[?&]before=([^&]+)/);
-        if(beforeMatch) { params.links.prev = `${currentUrl}?before=${beforeMatch[1]}`; }
-      }
-
-      const html = pug.renderFile('views/index.pug', params);
-      response.status(200).send(html);
-    });
-})
-
+// index
 app.get('/authors/:author_id/categories/:category/books', (req, res) => {
   const baseUrl = "https://api.bungomail.com/v0";
   let url = (req.params.author_id == 'all') ? `${baseUrl}/books` : `${baseUrl}/persons/${req.params.author_id}/books`;
@@ -46,10 +18,16 @@ app.get('/authors/:author_id/categories/:category/books', (req, res) => {
       return response.json();
     })
     .then((json) => {
+      const category = new Category(req.params.category);
+      const author = json.person || { 人物ID: 'all', 作品数: allBooksCount };
       let params = {
-        meta: metaData(json.person, req.params.category),
-        category: req.params.category,
-        author_id: req.params.author_id,
+        meta: {
+          title: title(author, category),
+          description: description(author, category)
+        },
+        category: category,
+        author: author,
+        allBooksCount: allBooksCount,
         links: {},
         results: json
       }
@@ -67,58 +45,101 @@ app.get('/authors/:author_id/categories/:category/books', (req, res) => {
     });
 })
 
-const metaData = (person, category) => {
-  const meta = {
-    title: title(person, category),
-    description: description(person, category)
-  };
-  return meta;
-}
 
-const title = (person, category) => {
-  const authorName = (person) ? person["姓名"] : '青空文庫';
+// show
+app.get('/authors/:author_id/categories/:category/books/:book_id', (req, res) => {
+  const url = `https://api.bungomail.com/v0/books/${req.params.book_id}`;
+  fetch(url)
+    .then((response) => {
+      if(!response.ok) { return res.status(500).send(response); }
+      return response.json();
+    })
+    .then((json) => {
+      const book = json.book;
+      const category = new Category(req.params.category);
+      const author = book;
+      let params = {
+        meta: {
+          title: `${book["姓名"]}『${book["作品名"]}』- ${category.readTime}で読める青空文庫の${category.name}作品 | ゾラサーチ`,
+          description: `『${book["作品名"]}』は青空文庫で公開されている${book["姓名"]}の${category.name}作品。${book["文字数"].toLocaleString()}文字で、おおよそ${category.readTime}で読むことができます。`
+        },
+        category: category,
+        author: author,
+        allBooksCount: allBooksCount,
+        links: {},
+        book: book
+      }
+      const html = pug.renderFile('views/show.pug', params);
+      res.status(200).send(html);
+    });
+});
+
+
+
+const title = (author, category) => {
+  const authorName = author["姓名"] || '青空文庫';
   let title = "";
-  if(category != 'all') {
-    title += `${categoryReadTime(category)}で読める`;
+  if(category.id != 'all') {
+    title += `${category.readTime}で読める`;
   }
-  title += `${authorName}の${categoryName(category)}作品`;
+  title += `${authorName}の${category.name}作品`;
   return title;
 }
 
-const description = (person, category) => {
-  const authorName = (person) ? person["姓名"] : 'すべて';
+const description = (author, category) => {
+  const authorName = author["姓名"] || 'すべての著者';
   let description = "";
   description += `青空文庫で公開されている${authorName}の作品`;
-  if(category != 'all') {
-    description += `の中で、おおよその読了目安時間が「${categoryReadTime(category)}」の${categoryName(category)}132作品`;
+  if(category.id != 'all') {
+    description += `の中で、おおよその読了目安時間が「${category.readTime}」の${category.name}作品`;
   }
-  description += `を、おすすめ人気順に表示しています。`;
+  const booksCount = (author['作品数'][category.id] || 0).toLocaleString();
+  description += `${booksCount}篇を、おすすめ人気順に表示しています。`;
   return description;
 }
 
-const categoryName = (id) => {
-  switch (id) {
-    case 'all': return '全';
-    case 'flash': return '短編';
-    case 'shortshort': return '短編';
-    case 'short': return '短編';
-    case 'novelette': return '中編';
-    case 'novel': return '長編';
-    default: return '';
-  }
+const allBooksCount = {
+ "all": 16353, "flash": 4633, "shortshort": 2581, "short": 4619, "novelette": 2488, "novel": 2032
 }
 
-const categoryReadTime = (id) => {
-  switch (id) {
-    case 'flash': return '5分以内';
-    case 'shortshort': return '10分以内';
-    case 'short': return '30分以内';
-    case 'novelette': return '60分';
-    case 'novel': return '1時間〜';
-    default: return '';
+
+class Category {
+  constructor (categoryId) {
+    this.id = categoryId;
+    switch(categoryId) {
+      case 'all':
+        this.name = '全';
+        this.readTime = 'すべて';
+        this.charsCount = 'すべて';
+        break;
+      case 'flash':
+        this.name = '短編';
+        this.readTime = '5分以内';
+        this.charsCount = '〜2,000文字';
+        break;
+      case 'shortshort':
+        this.name = '短編';
+        this.readTime = '10分以内';
+        this.charsCount = '2,000〜4,000文字';
+        break;
+      case 'short':
+        this.name = '短編';
+        this.readTime = '30分以内';
+        this.charsCount = '4,000〜12,000文字';
+        break;
+      case 'novelette':
+        this.name = '中編';
+        this.readTime = '60分以内';
+        this.charsCount = '12,000〜24,000文字';
+        break;
+      case 'novel':
+        this.name = '長編';
+        this.readTime = '1時間〜';
+        this.charsCount = '24,000文字〜';
+        break;
+    }
   }
 }
-
 
 
 exports.app = functions.https.onRequest(app);
